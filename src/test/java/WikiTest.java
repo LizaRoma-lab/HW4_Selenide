a2c9e1b07f19bbfb4177eda4bfa1b7558f880c9c
LizaRoma-lab <Elizaveta.Romanova.m@yandex.ru>
2025-03-12 22:23:57 +0300

    added WikiTest.java

--- src/test/java/WikiTest.java
+++ src/test/java/WikiTest.java
@@ -1,13 +1,46 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class WikiTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl =  "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000; // default 4000
+      //  Configuration.browser="firefox";
+    }
 
     @Test
     void softAssertionsIncludesJUnit() {
 
-        open("https://school.qa.guru/");
+        open("/selenide/selenide");
+        $("[id=wiki-tab]").click();
+
+        $$("ul li a").findBy(text("Soft Assertions")).click();
+        sleep(5000);
+
+        $(".markdown-body").shouldHave(text(
+                """
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                        
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }"""));
+        sleep(5000);
     }
 }
 
